c4c2e00ab5bcc3b99faaf8c048fd833d1e16649c
RadoslawDeska <69204738+RadoslawDeska@users.noreply.github.com>
2025-10-11 01:49:49 +0200

    Update main.py
    uzupełnienie

--- Moduł_2/main.py
+++ Moduł_2/main.py
@@ -1,3 +1,11 @@
+cubes = [i ** 3 for i in range(1,11)]
+for i in cubes:
+  if i % 2 != 0:
+    print(i, end=" ")
+print()
+
 listing = [2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2, 3, 0, 0]
 zeros = listing[1:4] + listing[5:-4] + listing[-2:]
 positive = listing[0:1] + listing[4:5] + listing[-4:-2]
+print(zeros)
+print(positive)
